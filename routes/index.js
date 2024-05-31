2dba3bd4e08e3209a70eacd1f79cf39dc8226013
Gallardo Lee <leegallardo16@gmail.com>
2024-05-31 18:44:12 +0700

    Render messages in index template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const datefns = require("date-fns");
 const router = express.Router();
 
-/* GET home page. */
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: datefns.format(new Date(), 'dd MMM yyyy')
+  },
+  {
+    text: "Hello world!",
+    user: "Charles",
+    added: datefns.format(new Date(), 'dd MMM yyyy')
+  }
+];
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages: messages  });
 });
 
 module.exports = router;
